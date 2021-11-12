5cc85af3564a5e7e2e628f16c8cfef99873d41ac
hasankhadra <hasankhadra2013@gmail.com>
2021-11-12 21:25:14 +0300

    Implemented website scraping using scrapy and xpath

--- Scrapy-basics/scrapy_basics/scrapy_basics/settings.py
+++ Scrapy-basics/scrapy_basics/scrapy_basics/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'scrapy_basics.spiders'
 #USER_AGENT = 'scrapy_basics (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
